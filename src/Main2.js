c39254ab2628fe544f1e05ce174bf0cb6c64961b
Armani Raleigh <armanijolee@gmail.com>
2016-10-11 16:15:23 -0400

    Adding more properties

---
+++ src/Main2.js
@@ -0,0 +1,20 @@
+//$(document).ready(function(){
+
+//  var img = $('#myPicture');
+//  //img.attr('src', 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png');
+//
+//  $("img").slideDown(12000);
+//  $('img').fadeTo(12000, 1);
+//    $("#first").click(function() {
+//    $("#first").css({
+//    'background-color': 'green'
+//    });
+//    });
+//   // $("img").mouseenter(function() {
+//       $("img").fadeTo("fast", 0.5);
+//       });
+//$("img").mouseleave(function() {
+//$("img").fadeTo("fast", 1.0);
+//});
+//
+//});
